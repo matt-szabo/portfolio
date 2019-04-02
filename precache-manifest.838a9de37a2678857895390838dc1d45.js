983f1b9202a7e3bc63e5c52e86bfd1db4e715f64
matt-szabo <mattidj@gmail.com>
2019-04-02 00:30:21 -0400

    Updates

---
+++ precache-manifest.838a9de37a2678857895390838dc1d45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2b08efa7cddf635e463",
+    "url": "/portfolio/static/css/main.453979f5.chunk.css"
+  },
+  {
+    "revision": "b2b08efa7cddf635e463",
+    "url": "/portfolio/static/js/main.b2b08efa.chunk.js"
+  },
+  {
+    "revision": "1d6de006d46bc0f4490b",
+    "url": "/portfolio/static/js/1.1d6de006.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4ad862bcd002e62a33f29229b3322623",
+    "url": "/portfolio/index.html"
+  }
+];
